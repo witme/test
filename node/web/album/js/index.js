79ba58ec915c6c4efdd29a7a2e60402ea5fbfca3
witme <luoweijiangcom@163.com>
2017-09-19 03:51:22 +0800

    add dom template.

--- node/web/album/js/index.js
+++ node/web/album/js/index.js
@@ -1,3 +1,37 @@
 $(function(){
+    var data = [
+        {
+        'galleryTitle': 'galleryTitle',
+        'figures': [
+            {
+                imgThumb: 'https://farm3.staticflickr.com/2567/5697107145_3c27ff3cd1_m.jpg',
+                image: 'https://farm3.staticflickr.com/2567/5697107145_a4c2eaa0cd_o.jpg',
+                caption: 'caption1'
+            },
+            {
+                imgThumb: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_m.jpg',
+                image: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
+                caption: 'caption2'
+
+            }],
+        'other':'other'
+        },
+    ];
+
+    var item = $('#item-dom-template').html();
+    var content = "";
+   
+    for(var k=0; k<data.length; k++){
+        var tmp = Mustache.render(item, {
+            galleryTitle: data[k].galleryTitle
+
+            
+        });
+        content += tmp;        
+    }
+    
+    $('#imgDom').append(content);
+
+    initPhotoSwipeFromDOM('.my-gallery');
 
 });
--- node/web/album/js/parse.js
+++ node/web/album/js/parse.js
@@ -202,4 +202,4 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 };
 
 // execute above function
-initPhotoSwipeFromDOM('.my-gallery');
+// initPhotoSwipeFromDOM('.my-gallery');
